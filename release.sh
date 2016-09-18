8c0dd4f19afe939508ed85b297a8d9b7650dd833
Christophe Liu <lsmgeb89@gmail.com>
2016-09-18 13:43:41 -0500

    [CS 6301][multicore][project_1] Add release script and a empty readme.txt

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+
+readonly NAME="multicore_project_1"
+readonly RELEASE_FOLDER="${HOME}/${NAME}"
+readonly RELEASE_ZIP="${HOME}/${NAME}.zip"
+
+# delete previous release zip
+if [ -f "$RELEASE_ZIP" ]; then
+  rm "$RELEASE_ZIP"
+fi
+
+mkdir -p "$RELEASE_FOLDER"/src
+# copy source files
+cp -ar src/* "$RELEASE_FOLDER"/src
+# copy readme.txt
+cp readme.txt "$RELEASE_FOLDER"
+# package all files
+pushd "${HOME}" > /dev/null 2>&1
+zip -r "$RELEASE_ZIP" "$NAME"/*
+chmod +x "$RELEASE_ZIP"
+popd > /dev/null 2>&1
+
+# delete release folder
+if [ -d "$RELEASE_FOLDER" ]; then
+  rm -rf "$RELEASE_FOLDER"
+fi
