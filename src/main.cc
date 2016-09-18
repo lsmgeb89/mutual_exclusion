e7149adb5d8bffc066e7797d9b82a13f7d19b43a
Christophe Liu <lsmgeb89@gmail.com>
2016-09-18 17:24:17 -0500

    [CS 6301][multicore][project_1] Enable multiple times for a test and take average

--- src/main.cc
+++ src/main.cc
@@ -5,40 +5,27 @@
 #include "tester.h"
 
 int main(int argc, char* argv[]) {
-  const std::size_t level(4);
   std::size_t thread_num(0);
   std::size_t loop_num(0);
+  std::size_t test_times(0);
 
-  if (argc != 3) {
+  if (argc != 4) {
     std::cerr << "Wrong Parameter!" << std::endl;
     return EXIT_FAILURE;
   } else {
     thread_num = static_cast<std::size_t>(std::stoul(argv[1]));
     loop_num = static_cast<std::size_t>(std::stoul(argv[2]));
-    if (thread_num > (1 << level)) {
-      std::cerr << "Too many threads!" << std::endl;
-      return EXIT_FAILURE;
-    }
+    test_times = static_cast<std::size_t>(std::stoul(argv[3]));
   }
 
-  {
-    utils::PetersonTester a(2, loop_num);
-    a.Test();
-  }
-
-  {
-    utils::TASTester<utils::TASLock> a(thread_num, loop_num);
-    a.Test();
-  }
-
-  {
-    utils::TASTester<utils::TTASLock> a(thread_num, loop_num);
-    a.Test();
-  }
-
-  {
-    utils::TourTester<level> a(thread_num, loop_num);
-    a.Test();
+  try {
+    utils::Tester t(thread_num, loop_num, test_times);
+    t.Test();
+    std::cout << t.ResultToString();
+  } catch (const std::runtime_error& err) {
+    std::cerr << "Mutual Exclusion failed!" << std::endl;
+  } catch (...) {
+    std::cerr << "Internal Error!" << std::endl;
   }
 
   return EXIT_SUCCESS;
--- src/utils/tester.h
+++ src/utils/tester.h
@@ -5,6 +5,7 @@
 #include <cstddef>
 #include <cstdint>
 #include <iostream>
+#include <sstream>
 #include <thread>
 #include <vector>
 
@@ -18,7 +19,9 @@ class BaseTester {
       thread_num_(thread_num),
       loop_num_(loop_num) {}
 
-  void Test(void) {
+  void SetThreadNum(const std::size_t& thread_num) { thread_num_ = thread_num; }
+
+  std::chrono::duration<double, std::nano> Test(void) {
     auto begin = std::chrono::steady_clock::now();
 
     CreateThreads();
@@ -29,10 +32,23 @@ class BaseTester {
 
     auto end = std::chrono::steady_clock::now();
 
+    if (counter_ != thread_num_ * loop_num_) {
+      throw std::runtime_error("Test failed");
+    }
+
     std::cout << "Thread = " << thread_num_ << ", Loop = " << loop_num_
               << ", Count = " << counter_ << ", Total Time = "
               << std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count()
               << " (ns)" << std::endl;
+
+    Reset();
+
+    return (end - begin);
+  }
+
+  void Reset(void) {
+    counter_ = 0;
+    thread_pool.clear();
   }
 
  private:
@@ -121,6 +137,75 @@ template <std::size_t LEVEL_NUM> class TourTester : public BaseTester {
 
  private:
   utils::TournamentLock<LEVEL_NUM, 1 << LEVEL_NUM> lock_;
+};
+
+class Tester {
+ public:
+  Tester(const std::size_t& thread_num,
+         const std::size_t& loop_num,
+         const std::size_t& test_times)
+    : thread_num_(thread_num),
+      loop_num_(loop_num),
+      test_times_(test_times),
+      tas_tester_(thread_num, loop_num),
+      ttas_tester_(thread_num, loop_num),
+      tour_tester_(thread_num, loop_num) {
+    tas_result_.resize(thread_num);
+    ttas_result_.resize(thread_num);
+    tour_result_.resize(thread_num);
+  }
+
+  void Test(void) {
+    for (std::size_t t_num = 1; t_num <= thread_num_; t_num++) {
+      tas_tester_.SetThreadNum(t_num);
+      ttas_tester_.SetThreadNum(t_num);
+      tour_tester_.SetThreadNum(t_num);
+
+      // For each thread number, we test multiple times
+      for (std::size_t i = 0; i < test_times_; i++) {
+        tas_result_.at(t_num - 1) += tas_tester_.Test();
+        ttas_result_.at(t_num - 1) += ttas_tester_.Test();
+        tour_result_.at(t_num - 1) += tour_tester_.Test();
+      }
+
+      // average
+      tas_result_.at(t_num - 1) /= test_times_;
+      ttas_result_.at(t_num - 1) /= test_times_;
+      tour_result_.at(t_num - 1) /= test_times_;
+    }
+  }
+
+  std::string ResultToString(void) {
+    std::stringstream out_stream;
+    for (auto item : tas_result_) {
+      out_stream << item.count() << ", ";
+    }
+    out_stream << std::endl;
+    for (auto item : ttas_result_) {
+      out_stream << item.count() << ", ";
+    }
+    out_stream << std::endl;
+    for (auto item : tour_result_) {
+      out_stream << item.count() << ", ";
+    }
+    out_stream << std::endl;
+
+    return out_stream.str();
+  }
+
+ private:
+  static constexpr std::size_t level = 4;
+  std::size_t thread_num_;
+  std::size_t loop_num_;
+  std::size_t test_times_;
+
+  utils::TASTester<utils::TASLock> tas_tester_;
+  utils::TASTester<utils::TTASLock> ttas_tester_;
+  utils::TourTester<level> tour_tester_;
+
+  std::vector<std::chrono::duration<double, std::nano>> tas_result_;
+  std::vector<std::chrono::duration<double, std::nano>> ttas_result_;
+  std::vector<std::chrono::duration<double, std::nano>> tour_result_;
 };
 
 } // namespace utils
