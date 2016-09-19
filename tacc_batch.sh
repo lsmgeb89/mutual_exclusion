30093ac0614fe004f5683c3d56f65ff2429b99a1
Christophe Liu <lsmgeb89@gmail.com>
2016-09-19 17:02:31 -0500

    [CS 6301][multicore][project_1] Add a script for TACC batch session and tweak compile flag

---
+++ tacc_batch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J lock_compare # job name
+#SBATCH -o lock_compare_result_%j.txt # output and error file name (%j=jobID)
+#SBATCH -n 16 # total number of cpus requested
+#SBATCH -p development # queue -- normal, development, etc.
+#SBATCH -t 02:00:00 # run time (hh:mm:ss) - 2.0 hours
+#SBATCH --mail-user=siming.liu@utdallas.edu
+#SBATCH --mail-type=begin # email me when the job starts
+#SBATCH --mail-type=end # email me when the job finishes
+
+${HOME}/multicore_project_1/src/lock_compare 16 1000000 16
+
