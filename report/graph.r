bdfc9d204223838bd25c8630639debf51610bb15
Christophe Liu <lsmgeb89@gmail.com>
2016-09-21 21:14:00 -0500

    [CS 6301][multicore][project_1] Add parameter handling in the R script

--- report/graph.r
+++ report/graph.r
@@ -1,13 +1,18 @@
 ### load package
 library(ggplot2)
 
+### get parameter
+options <- commandArgs(trailingOnly = TRUE)
+
+name <- sub("*.txt", "", options)
+
 ### load data
-time_data = read.table("result.txt", header = TRUE, sep = ",", skip = 1)
+time_data = read.table(options, header = TRUE, sep = ",", skip = 1)
 
 ### plot
 attach(time_data)
 setEPS()
-postscript("result.eps")
+postscript(paste(name, ".eps", sep = ""))
 ggplot() +
   geom_line(data = time_data, aes(x = time_data$ThreadNumber, y = time_data$TASLock, colour = "TASLock")) +
   geom_line(data = time_data, aes(x = time_data$ThreadNumber, y = time_data$TTASLock, colour = "TTASLock")) +
